59fb24ca1cc64df5de8d38e5e5cec21bc9b8ead3
amandat <amandatome@me.com>
2019-04-15 05:33:13 -0400

    Started form validation

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,40 +1,74 @@
-$(".submit").on("click", function(event) {
+let isValid = true;
+
+$("#submitBtn").on("click", function(event) {
     event.preventDefault();
+//FORM VALIDATION
+function validateForm(){
+  $(".form-control").each(function(){
+    if($(this).val() === ""){
+      isValid = false;
+      console.log(this)
+    }
+  })
+}
+validateForm();
 
-    // Here we grab the form elements
-    let newUsers= {
-      customerName: $("#reserve-name").val().trim(),
-      phoneNumber: $("#reserve-phone").val().trim(),
-      customerEmail: $("#reserve-email").val().trim(),
-      customerID: $("#reserve-unique-id").val().trim()
-    };
+// if()
 
-    console.log(newUsers);
+//IF VALID
+  //Get value of name and photo
+  //Loop through chosen answers
+  //Push answers into array in object, push photo and name into object
 
-    // This line is the magic. It"s very similar to the standard ajax function we used.
-    // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
-    // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
-    // depending on if a tables is available or not.
+//POST TO JSON WITH $POST
 
-    $.post("/api/users", newUsers,
-      function(data) {
+//CLEAR USER INPUT ON SUBMIT
 
-        // If a table is available... tell user they are booked.
-        // if (data) {
-        //   alert("Yay! You are officially booked!");
-        // }
+//COMPARE USER RESULTS TO JSON RESULTS
 
-        // If a table is available... tell user they on the waiting list.
-        // else {
-        //   alert("Sorry you are on the wait list");
-        // }
+//MODAL  POP OF MATCH
 
-        // Clear the form when submitting
-        $("#reserve-name").val("");
-        $("#reserve-phone").val("");
-        $("#reserve-email").val("");
-        $("#reserve-unique-id").val("");
 
-      });
+//WRITE IN VANILLA JS
+//REMEMBER TO LINK THIS FILE!!!!!!
+
+
+
+
+  //   // Here we grab the form elements
+  //   let newUsers= {
+  //     customerName: $("#reserve-name").val().trim(),
+  //     phoneNumber: $("#reserve-phone").val().trim(),
+  //     customerEmail: $("#reserve-email").val().trim(),
+  //     customerID: $("#reserve-unique-id").val().trim()
+  //   };
+
+  //   console.log(newUsers);
+
+  //   // This line is the magic. It"s very similar to the standard ajax function we used.
+  //   // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
+  //   // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
+  //   // depending on if a tables is available or not.
+
+  //   $.post("/api/users", newUsers,
+  //     function(data) {
+
+  //       // If a table is available... tell user they are booked.
+  //       // if (data) {
+  //       //   alert("Yay! You are officially booked!");
+  //       // }
+
+  //       // If a table is available... tell user they on the waiting list.
+  //       // else {
+  //       //   alert("Sorry you are on the wait list");
+  //       // }
+
+  //       // Clear the form when submitting
+  //       $("#reserve-name").val("");
+  //       $("#reserve-phone").val("");
+  //       $("#reserve-email").val("");
+  //       $("#reserve-unique-id").val("");
+
+  //     });
 
   });
